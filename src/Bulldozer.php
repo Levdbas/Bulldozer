ccfb3e01aa956273589fba16edf4ccbf59c9e4f3
Erik van der Bas <erik@basedonline.nl>
2024-12-05 14:43:21 +0100

    5.2.1

--- src/Bulldozer.php
+++ src/Bulldozer.php
@@ -21,7 +21,7 @@ class Bulldozer
     /**
      * Current Bulldozer version.
      */
-    public const VERSION = '5.2.0';
+    public const VERSION = '5.2.1';
 
     /**
      * Active theme object.
