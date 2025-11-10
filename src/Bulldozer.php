c4459e0405363a584b25629f4cfcd32676e9c50b
Erik van der Bas <erik@basedonline.nl>
2025-11-10 17:37:04 +0100

    fix: Update Bulldozer version to 5.8.2

--- src/Bulldozer.php
+++ src/Bulldozer.php
@@ -21,7 +21,7 @@ class Bulldozer
     /**
      * Current Bulldozer version.
      */
-    public const VERSION = '5.8.1';
+    public const VERSION = '5.8.2';
 
     /**
      * Active theme object.
